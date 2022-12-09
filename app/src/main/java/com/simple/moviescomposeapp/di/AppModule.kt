21e7533b250aa4eba3662d58595d19aa9d5726e5
viktor.grigorov <viktor_grigorov@abv.bg>
2022-12-09 14:31:18 +0200

    Added provide fun for okHttpClient, retrofit and MovieApi in AppModule.kt, and now we inject it in MoviesRepositoryImpl.kt

--- app/src/main/java/com/simple/moviescomposeapp/di/AppModule.kt
+++ app/src/main/java/com/simple/moviescomposeapp/di/AppModule.kt
@@ -1,12 +1,19 @@
 package com.simple.moviescomposeapp.di
 
+import com.simple.moviescomposeapp.data.MoviesAPI
 import com.simple.moviescomposeapp.data.repository_impl.MoviesRepositoryImpl
 import com.simple.moviescomposeapp.domain.repository.MovieRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
+
+private const val BASE_URL = "https://api.themoviedb.org/3/"
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -15,5 +22,29 @@ class AppModule {
     @Provides
     @Singleton
     fun provideMoviesRepository(repository: MoviesRepositoryImpl): MovieRepository = repository
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(
+                HttpLoggingInterceptor()
+                    .setLevel(HttpLoggingInterceptor.Level.BODY)
+            )
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(httpClient: OkHttpClient): Retrofit =
+        Retrofit.Builder().baseUrl(BASE_URL)
+            .client(httpClient)
+            .addConverterFactory(
+                GsonConverterFactory.create()
+            )
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideApi(retrofit: Retrofit): MoviesAPI = retrofit.create(MoviesAPI::class.java)
 
 }
