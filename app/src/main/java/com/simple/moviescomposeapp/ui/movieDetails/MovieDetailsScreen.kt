c7917c6ad3c8e19f5e21b0b6d9b46a3bf318c5b6
A20Karlikov <al.karlikov@gmail.com>
2022-12-12 15:04:58 +0200

    Changes:  - create MovieInfo Composable  - refactor MovieDetailsScreen.kt  - merge remote develop

--- app/src/main/java/com/simple/moviescomposeapp/ui/movieDetails/MovieDetailsScreen.kt
+++ app/src/main/java/com/simple/moviescomposeapp/ui/movieDetails/MovieDetailsScreen.kt
@@ -29,6 +29,7 @@ import coil.compose.rememberAsyncImagePainter
 import com.simple.moviescomposeapp.R
 import com.simple.moviescomposeapp.R.string
 import com.simple.moviescomposeapp.data.models.toValuesList
+import com.simple.moviescomposeapp.ui.CustomTopAppBar
 import com.simple.moviescomposeapp.ui.theme.Shapes
 import java.text.SimpleDateFormat
 
@@ -37,95 +38,28 @@ fun MovieDetailsScreen() {
     val viewModel: MovieDetailsViewModel = viewModel()
     val currentMovie = viewModel.currentMovie.value
 
-    Box(contentAlignment = Alignment.BottomCenter) {
-        Card(shape = Shapes.large) {
-            Image(
-                painter = rememberAsyncImagePainter(currentMovie?.imageUrl),
-                contentDescription = null,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .fillMaxHeight(),
-                contentScale = ContentScale.Crop
-            )
-        }
-        Surface(
-            modifier = Modifier
-                .padding(16.dp)
-                .fillMaxWidth(),
-            shape = RoundedCornerShape(6.dp),
-            color = Color.White
-        ) {
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier.verticalScroll(rememberScrollState())
-            ) {
-                Text(
-                    text = currentMovie?.title ?: stringResource(string.no_title_message),
-                    style = MaterialTheme.typography.h4,
+    Column {
+        CustomTopAppBar()
+
+        Box(contentAlignment = Alignment.BottomCenter) {
+            Card(shape = Shapes.large) {
+                Image(
+                    painter = rememberAsyncImagePainter(currentMovie?.imageUrl),
+                    contentDescription = null,
                     modifier = Modifier
-                        .padding(horizontal = 12.dp, vertical = 16.dp)
                         .fillMaxWidth()
-                        .wrapContentHeight()
+                        .fillMaxHeight(),
+                    contentScale = ContentScale.Crop
                 )
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.SpaceEvenly,
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    Column {
-                        SingleInfoElement(
-                            elementName = stringResource(string.movie_detail_release_date),
-                            value = currentMovie?.releaseDate ?: stringResource(string.no_info_provided),
-                            drawableId = R.drawable.ic_release_date
-                        )
-                        Spacer(modifier = Modifier.padding(8.dp))
-                        SingleInfoElement(
-                            elementName = stringResource(string.movie_detail_rating),
-                            value = currentMovie?.voteAverage.toString(),
-                            drawableId = R.drawable.ic_rating
-                        )
-                    }
-                    Column {
-                        SingleInfoElement(
-                            elementName = stringResource(string.movie_detail_budget),
-                            value = "${currentMovie?.budget.toString()}M",
-                            drawableId = R.drawable.ic_budget
-                        )
-                        Spacer(modifier = Modifier.padding(8.dp))
-                        SingleInfoElement(
-                            stringResource(string.movie_detail_duration),
-                            value = "${currentMovie?.duration.toString()} min",
-                            drawableId = R.drawable.ic_duration
-                        )
-                    }
-                }
-                currentMovie?.genres?.run {
-                    Text(
-                        text = this.toValuesList().joinToString(", "),
-                        modifier = Modifier
-                            .padding(top = 16.dp, start = 12.dp, end = 12.dp, bottom = 4.dp)
-                            .fillMaxWidth()
-                            .wrapContentHeight(),
-                        style = MaterialTheme.typography.subtitle2
-                    )
-                }
-                currentMovie?.overview?.run {
-                    Text(
-                        text = this@run,
-                        modifier = Modifier
-                            .padding(horizontal = 12.dp)
-                            .fillMaxWidth(),
-                        style = MaterialTheme.typography.subtitle1,
-                        fontSize = 18.sp,
-                    )
-                }
-                Button(modifier = Modifier
-                    .fillMaxWidth(0.35F)
-                    .padding(vertical = 16.dp),
-                    onClick = { /*TODO*/ }
-                ) {
-                    Text(text = stringResource(string.suggest_movie_btn))
-                }
+            }
+            Surface(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth(),
+                shape = RoundedCornerShape(6.dp),
+                color = Color.White
+            ) {
+                currentMovie?.run { MovieInfo(movie = currentMovie) }
             }
         }
     }
