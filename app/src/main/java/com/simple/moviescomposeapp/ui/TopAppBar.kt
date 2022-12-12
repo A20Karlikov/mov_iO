efef931af743cbb2fd447b56effa34d753d1c93f
viktor.grigorov <viktor_grigorov@abv.bg>
2022-12-12 11:06:12 +0200

    Created toolbar, added files to git ignore

--- app/src/main/java/com/simple/moviescomposeapp/ui/TopAppBar.kt
+++ app/src/main/java/com/simple/moviescomposeapp/ui/TopAppBar.kt
@@ -5,19 +5,15 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.simple.moviescomposeapp.R
 import com.simple.moviescomposeapp.ui.theme.MoviesComposeAppTheme
 
 @Composable
@@ -36,8 +32,7 @@ fun CustomTopAppBar() {
                     .padding(8.dp),
                 textAlign = TextAlign.Center
             )
-        }
-        ,
+        },
         modifier = Modifier
             .fillMaxWidth(),
         backgroundColor = Color.Black,
--- app/src/main/java/com/simple/moviescomposeapp/ui/moviesList/MoviesListScreen.kt
+++ app/src/main/java/com/simple/moviescomposeapp/ui/moviesList/MoviesListScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.simple.moviescomposeapp.ui.CustomTopAppBar
 
 @Composable
 fun MoviesListScreen() {
