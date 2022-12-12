4ef9e47ee6484533f3bbbc65a79a280108507c78
viktor.grigorov <viktor_grigorov@abv.bg>
2022-12-12 11:39:14 +0200

    Centered application title in the toolbar and added the title to strings.xml

--- app/src/main/java/com/simple/moviescomposeapp/ui/TopAppBar.kt
+++ app/src/main/java/com/simple/moviescomposeapp/ui/TopAppBar.kt
@@ -2,18 +2,20 @@ package com.simple.moviescomposeapp.ui
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.SnackbarDefaults.backgroundColor
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.simple.moviescomposeapp.R
 import com.simple.moviescomposeapp.ui.theme.MoviesComposeAppTheme
 
 @Composable
@@ -21,17 +23,24 @@ fun CustomTopAppBar() {
     TopAppBar(
         title =
         {
-            Spacer(modifier = Modifier.padding(68.dp))
-            Text(
-                "moviO",
+            Row(
+                verticalAlignment = Alignment.CenterVertically ,
+                horizontalArrangement = Arrangement.Center,
                 modifier = Modifier
-                    .border(
-                        BorderStroke(1.dp, Color.White),
-                        shape = CircleShape
-                    )
-                    .padding(8.dp),
-                textAlign = TextAlign.Center
-            )
+                    .fillMaxSize()
+                    .padding(end = 12.dp)
+            ) {
+                Text(
+                    stringResource(R.string.movio_title),
+                    modifier = Modifier
+                        .border(
+                            BorderStroke(1.dp, Color.White),
+                            shape = CircleShape
+                        )
+                        .padding(8.dp),
+                    textAlign = TextAlign.Center
+                )
+            }
         },
         modifier = Modifier
             .fillMaxWidth(),
